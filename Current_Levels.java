306fbe31da551b5709193f094c2c24c05d0be85c
mpgodwin <31316575+mpgodwin@users.noreply.github.com>
2017-11-01 22:15:33 -0400

    Add files via upload
    Skeleton of my portion of the project with simple algorithm for organization sake

---
+++ Current_Levels.java
@@ -0,0 +1,19 @@
+package application;
+
+
+//Display the current levels to the user. Then determine if those levels are in the safe zone. 
+//If the levels are outside of the safe zone- need to calculate insulin to insert in patient.
+
+//**** ALGORITHM ***
+//  set current_Level to equal 0.
+//	Then read in the new current level value
+// 	Display the current level
+// 	Send the current level to the Emergency level class to determine if insulin is needed
+
+
+public class Current_Levels {
+
+	int current_Level = 0;
+	 
+	
+}
