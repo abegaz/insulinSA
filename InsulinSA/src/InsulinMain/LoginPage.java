3110a283cdd3a66b211aed62fe70ff7da968c2f3
John Michael Farina <jfari1364@ung.edu>
2017-11-06 20:47:50 -0500

    Entire Patient/Physician Login Folder

--- InsulinSA/src/InsulinMain/LoginPage.java
+++ InsulinSA/src/InsulinMain/LoginPage.java
@@ -2,34 +2,51 @@ package InsulinMain;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+
 public class LoginPage extends Application {
-	private Stage primaryStage;
-	private AnchorPane DrLoginPage;
-
+	private double xOffset = 0;
+	private double yOffset = 0;
 	@Override
-	public void start(Stage primaryStage) throws IOException {
-		FXMLLoader loader = new FXMLLoader (getClass().getResource("DrLoginPage.fxml"));
-		DrLoginPage = loader.load();
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Insulin Pump");
-		showLoginView();		
+	public void start(Stage stage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+		stage.setTitle("Insulin Pump");
+	    stage.getIcons().add(new Image("file:src/InsulinMain/blueHeartbeat.png"));
+        stage.setResizable(false);
+    
+	
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+        	@Override
+        	public void handle (MouseEvent event) {
+        		xOffset = event.getSceneX();
+        		yOffset = event.getSceneY();
+        	}
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+        	@Override
+			public void handle (MouseEvent event) {
+        		stage.setX(event.getScreenX() - xOffset);
+        		stage.setY(event.getScreenY() - yOffset);
+        	}
+        });
+        
+        Scene scene = new Scene (root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
 	}
-
-
-	private void showLoginView() throws IOException {
-		Scene scene = new Scene (DrLoginPage);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
-	}
+ 
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
