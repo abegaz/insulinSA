58a26608af498870af7ad22031c96f87d75d4d7c
juanp3053 <juanp3053@gmail.com>
2017-11-08 18:01:49 -0500

    Update on Project
    I have joined the different views and controllers I found around the github, Most of the panes are now connected to each other (you can navigate now)

---
+++ Java/JP/InsulinA/src/application/MainApp.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class MainApp extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent  root = FXMLLoader.load(getClass().getResource("../view/PatientMainView.fxml"));
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
