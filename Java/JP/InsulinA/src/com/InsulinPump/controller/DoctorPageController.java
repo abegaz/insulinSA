f2ad65977f416feec8010419efbb9ce32a99608b
juanp3053 <juanp3053@gmail.com>
2017-11-11 12:51:47 -0500

    Updated DELETE DoctorPage

--- Java/JP/InsulinA/src/com/InsulinPump/controller/DoctorPageController.java
+++ Java/JP/InsulinA/src/com/InsulinPump/controller/DoctorPageController.java
@@ -1,24 +1,148 @@
 package com.InsulinPump.controller;
 
+import com.InsulinPump.model.*;
+import application.InsulinPumpDBConfig;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 public class DoctorPageController {
-	@FXML
-	private Button addPatient;
+	
+	//configure the Patient table view
+		@FXML private TableView<Patient> tableViewInfo;
+	    @FXML private TableColumn<Patient, String> cPatientId;
+	    @FXML private TableColumn<Patient, String> cFirstName;
+	    @FXML private TableColumn<Patient, String> cLastName;
+	    @FXML private TableColumn<Patient, String> cAddress;
+	    @FXML private TableColumn<Patient, String> cBloodType;
+	    @FXML private TableColumn<Patient, String> cAge;
+	    @FXML private TableColumn<Patient, String> cGender;
+	    @FXML private TableColumn<Patient, String> cHeight;
+	    @FXML private TableColumn<Patient, String> cWeight;
+	    @FXML private TableColumn<Patient, String> cInsulinType;
+	    @FXML private TableColumn<Patient, String> cPhone;
+	    @FXML private TableColumn<Patient, String> cDoctor;
+	    
+	    @FXML private Button addPatient;
+	    @FXML private Button bDeletePatient;
+	
+	@FXML public void initialize() {
+		
+        //set up the columns in the table
+		cPatientId.setCellValueFactory(new PropertyValueFactory<Patient, String>("patientID"));
+		cFirstName.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstName"));
+		cLastName.setCellValueFactory(new PropertyValueFactory<Patient, String>("lastName"));
+		cAddress.setCellValueFactory(new PropertyValueFactory<Patient, String>("address"));
+		cBloodType.setCellValueFactory(new PropertyValueFactory<Patient, String>("bloodType"));
+		cAge.setCellValueFactory(new PropertyValueFactory<Patient, String>("age"));
+		cGender.setCellValueFactory(new PropertyValueFactory<Patient, String>("gender"));
+		cHeight.setCellValueFactory(new PropertyValueFactory<Patient, String>("height"));
+		cWeight.setCellValueFactory(new PropertyValueFactory<Patient, String>("weight"));
+		cInsulinType.setCellValueFactory(new PropertyValueFactory<Patient, String>("insulinType"));
+		cPhone.setCellValueFactory(new PropertyValueFactory<Patient, String>("phone"));
+		cDoctor.setCellValueFactory(new PropertyValueFactory<Patient, String>("doctorID"));
+		
+        //static data, will replace with the database record later
+		tableViewInfo.setItems(getPatientList());
+		
+        /*//Update the table to allow for the first and last name fields to be editable
+		tableViewInfo.setEditable(true);
+        firstNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        addressColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        */
+        //This will allow the table to select multiple rows at once
+		tableViewInfo.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        //Disable the detailed Customer view button and the delete button until a row is selected
+        //this.detailedCustomerViewButton.setDisable(true);
+        this.bDeletePatient.setDisable(true);
+	}
+	
+	// This method removes one or more selected row(s) from the TableView
+    public void deleteButtonPushed(){
+    	
+        ObservableList<Patient> selectedRows, allPatient;
+        allPatient = tableViewInfo.getItems();
 
-	// Event Listener on Button[#addPatient].onAction
+        //this gives us the rows that were selected
+        selectedRows = tableViewInfo.getSelectionModel().getSelectedItems();
+
+        //loop over the selected rows and remove the Patient objects from the table
+        for (Patient patient: selectedRows)
+        {
+        	allPatient.remove(patient);
+        	String query = "DELETE FROM patient where idPatient = ?;";
+
+        	try(
+        	    Connection conn = InsulinPumpDBConfig.getConnection();
+        	    PreparedStatement updateprofile = conn.prepareStatement(query);
+        	){
+        	updateprofile.setString(1, patient.getPatientId());
+        	    updateprofile.executeUpdate();
+        	} catch (Exception e) {
+    			System.out.println("Status: operation failed due to "+e);
+
+    		}
+        }
+    }
+    
+ // ObservableList: A list that enables listeners to track changes when they occur
+    // The following  method will return an ObservableList of  object
+    public ObservableList<Patient>  getPatientList(){
+    	
+    	ObservableList<Patient> patient = FXCollections.observableArrayList();
+
+        String SQLQuery = "SELECT idPatient, firstName, lastName, Address, bloodType, age, gender, height, weight, insulinType, phone, idDoctor FROM patient ORDER BY idPatient ASC;"; //ADD WHERE idPatient == ''
+       	ResultSet rs = null;
+
+       	try(
+       			Connection conn = InsulinPumpDBConfig.getConnection();
+       			PreparedStatement displayprofile = conn.prepareStatement(SQLQuery);
+       	){
+       		//displayprofile.setInt(1, cutomerId);
+       		rs = displayprofile.executeQuery();
+       		// check to see if receiving any data
+       		while (rs.next()){
+       			patient.add(new Patient(rs.getString("idPatient").toString(),rs.getString("firstName").toString(),rs.getString("lastName").toString(),rs.getString("Address").toString(),rs.getString("bloodType").toString(),
+       					rs.getString("age").toString(),rs.getString("gender").toString(),rs.getString("height").toString(),rs.getString("weight").toString(),rs.getString("insulinType").toString(),rs.getString("phone").toString(),rs.getString("idDoctor").toString()));
+       		}
+       	}catch(SQLException ex){
+       		InsulinPumpDBConfig.displayException(ex);
+       		return null;
+       	}finally{
+       		if(rs != null){
+       			//rs.close();
+       		}
+       	}
+        return patient;
+    }
+    
+    // Method used to enable the detailed view button on mouse click event
+    public void mouseClickedOnTableView(){
+         	this.bDeletePatient.setDisable(false);
+           //this.deleteCustomerButton.setDisable(false);
+       }
+	
 	@FXML
 	public void changeSceneToPatientInsert(ActionEvent event) throws IOException {
 			if(event.getSource() == addPatient) {
