cf4fc8a12396532c4a9494f2138a3fdccced937b
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2017-11-10 20:09:59 -0500

    Update Timers.java

--- Java/JP/InsulinA/src/com/InsulinPump/controller/Timers.java
+++ Java/JP/InsulinA/src/com/InsulinPump/controller/Timers.java
@@ -9,6 +9,6 @@ public class Timers extends TimerTask{
 	//this method performs the task
 	public void run () {
 	// need to automatically input yes in the test controller for check bs.
-	Testcontroller.runPump();
+	//PatientMainMenuController.runPump();
 	}
 }
